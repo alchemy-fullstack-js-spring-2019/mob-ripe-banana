cdf7951358810a52af4711f0f4ff5ef1dc0ff2f0
Tommy Tran <tranttommy95@gmail.com>
2019-04-18 13:22:23 -0700

    error and not found middleware

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
 const app = require('./lib/app');
+const mongoose = require('mongoose');
 
-app.listen(7890, () => {
-  console.log('Server started on 7890.');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}.`);
 });
