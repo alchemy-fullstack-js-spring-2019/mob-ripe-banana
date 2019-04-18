5868a8786011eaf3e148026c758ea1e28027eac1
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-18 16:55:51 -0700

    actor seed data

--- test/seed-data.js
+++ test/seed-data.js
@@ -1,7 +1,8 @@
 const chance = require('chance').Chance();
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 
-module.exports = ({ studioCount = 5 } = {}) => {
+module.exports = ({ studioCount = 5, actorCount = 5 } = {}) => {
   const studios = [...Array(studioCount)].map(() => ({
     name: 'Studio',
     address: {
@@ -11,6 +12,14 @@ module.exports = ({ studioCount = 5 } = {}) => {
     }
   }));
 
-  return Studio
-    .create(studios);
+  const actors = [...Array(actorCount)].map(() => ({
+    name: 'Ryan Gosling',
+    dob: chance.date(),
+    pob: chance.city()
+  }));
+
+  return Promise.all([
+    Studio.create(studios),
+    Actor.create(actors)
+  ]);
 };
