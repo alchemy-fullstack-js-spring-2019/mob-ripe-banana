e95a2906567ec451e2e1823758fd7ff5b5d6db2e
UncleJerry <jared.myhrberg@gmail.com>
2019-04-18 14:47:27 -0700

    studio findById test passing

--- test/e2e/studioRoutes.test.js
+++ test/e2e/studioRoutes.test.js
@@ -4,6 +4,14 @@ const request = require('supertest');
 const StudioSchema = require('../../lib/models/StudioSchema');
 const chance = require('chance')();
 
+function getStudio() {
+  return StudioSchema
+    .create({
+      name: 'StudioOne',
+      address: { state: chance.state(), city: chance.city(), country: chance.country() }
+    });
+}
+
 describe('studio routes', () => {
   beforeAll(() => {
     return mongoose.connect('mongodb://localhost:27017/warehouses', {
@@ -44,18 +52,31 @@ describe('studio routes', () => {
   });
 
   it('can get a list of studios', () => {
-    return StudioSchema
-      .create({
-        name: 'StudioOne',
-        address: { state: chance.state(), city: chance.city(), country: chance.country() }
-      })
+    return getStudio()
       .then(() => {
         return request(app)
           .get('/api/v1/studios')
           .then(studiosList => {
             expect(studiosList.body).toHaveLength(1);
           });
+      });    
+  });
+
+  it('gets studio by id', () => {
+    return getStudio()
+      .then(createdStudio => {
+        return request(app)
+          .get(`/api/v1/studios/${createdStudio._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              name: 'StudioOne',
+              address: { state: expect.any(String), city: expect.any(String), country: expect.any(String) },
+              _id: expect.any(String)
+            });
+          });
       });
+
+          
   });
 
 });
