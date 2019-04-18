0d113cc05687f9d0772a1e8655abb39cbbe9598c
Leslie Shea <leslieyshea@gmail.com>
2019-04-18 13:28:53 -0700

    setup server and app

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./lib/app');
+
+const PORT = process.env.PORT || 7890;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+app.listen(PORT, () => {
+  /*eslint-disable-next-line no-console*/
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
