4ffcc5eebb2d9fcab90bdbfa6f7d08b66c84398e
Tommy Tran <tranttommy95@gmail.com>
2019-04-18 14:21:41 -0700

    required name field test passed

--- test/models/Studio.test.js
+++ test/models/Studio.test.js
@@ -22,4 +22,17 @@ describe('Studio Model Tests', () => {
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
+  it('requires a name field', () => {
+    const studio = new Studio({
+      address: {
+        city: 'Portland',
+        state: 'OR',
+        country: 'USA'
+      }
+    });
+
+    const error = studio.validateSync().errors;
+  
+    expect(error.name.message).toEqual('Path `name` is required.');
+  });
 });
