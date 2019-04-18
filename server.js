d1dc5c1b970b388dcee425b7f4d0b56894d5d9f4
UncleJerry <jared.myhrberg@gmail.com>
2019-04-18 14:15:41 -0700

    passed studio POST route

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
+//dotenv
 
 const app = require('./lib/app');
 const mongoose = require('mongoose');
-
 
 mongoose.connect('mongodb://localhost:27017/Warehouses', {
   useFindAndModify: false,
---
+++ test/e2e/studioRoutes.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const app = require('../../lib/app');
+const request = require('supertest');
+const StudioSchema = require('../../lib/models/StudioSchema');
+const chance = require('chance')();
+
+describe('studio routes', () => {
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/warehouses', {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+
+  it('can create a studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'StudioOne',
+        address: { state: chance.state(), city: chance.city(), country: chance.country() }
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'StudioOne',
+          address: { 
+            state: expect.any(String), 
+            city: expect.any(String), 
+            country: expect.any(String) 
+          },
+          _id: expect.any(String),
+          __v: 0,
+        });
+      });
+  });
+
+});
