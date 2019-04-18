2cc94f20bd89b583a246fd297c219e75ee8d4ce2
Leslie Shea <leslieyshea@gmail.com>
2019-04-18 16:13:52 -0700

    added actors

---
+++ test/routes/actors.test.js
@@ -0,0 +1,86 @@
+const request = require('supertest');
+const Actor = require('../../lib/models/Actor');
+const mongoose = require('mongoose');
+const app = require('../../lib/app');
+
+describe('actors routes', () => {
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/actors-test', {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a new actor', () => {
+    return request(app)
+      .post('/actors')
+      .send({
+        name: 'Carol',
+        dob: 2,
+        pob: 'Texas',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Carol',
+          dob: 2,
+          pob: 'Texas',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('can get list a of actors', () => {
+    return Actor.create({
+      name: 'Toddman'
+    })
+      .then(() => {
+        return request(app)
+          .get('/actors'); 
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+
+  it('can get a actor by id', () => {
+    return Actor.create({
+      name: 'Toddman'
+    })
+      .then(createdActor => {
+        return request(app)
+          .get(`/actors/${createdActor._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Toddman',
+          _id: expect.any(String)
+        });
+      });
+  });
+  it('can delete a actor by ID', () => {
+    return Actor.create({
+      name: 'Warner Bros'
+    })
+      .then(createdActor => {
+        return Promise.all([
+          Promise.resolve(createdActor),
+          request(app)
+            .delete(`/actors/${createdActor._id}`)
+        ]);
+      })
+      .then(([createdActor, res]) => {
+        expect(res.body._id).toEqual(createdActor._id.toString());
+      });
+  });
+});
+
