1a3f47eef96afe713f8b0109218a313b74352b72
Parker Helland <hellandp@gmail.com>
2019-04-18 14:11:44 -0700

    created and passed test for post

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,27 @@
-describe('', () => {
-  it('', () => {
-    
+const request = require('supertest');
+// const mongoose = require('mongoose');
+const app = require('../lib/app');
+// const Actor = require('../lib/Models/Actor');
+require('./data-helpers');
+
+describe('actor routes', () => {
+  const date = new Date;
+  it('can create a new actor', () => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'name',
+        dob: date,
+        pob: 'pb'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'name',
+          dob: date.toISOString(),
+          pob: 'pb',
+          _id: expect.any(String),
+          __v: 0
+        });
+      });
   });
 });
