3c6f49e4806667960b1a569cd6fbb71ade69843b
Anna Ridenour <annamridenour@gmail.com>
2019-04-19 10:33:35 -0700

    passed first review test

--- test/models/Review.test.js
+++ test/models/Review.test.js
@@ -11,6 +11,15 @@ describe('Review model', () => {
             review: 'this film was great',
             film: anotherId
         });
+        expect(review.toJSON()).toEqual({
+            rating: 4,
+            reviewer: id,
+            review: 'this film was great',
+            film: anotherId,
+            createdAt: expect.anything(),
+            updatedAt: expect.anything(),
+            _id: expect.any(mongoose.Types.ObjectId)
+        });
 
     });
 });
