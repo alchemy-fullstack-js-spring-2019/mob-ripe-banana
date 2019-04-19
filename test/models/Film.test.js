3b95f7ff96e8b388270c5ad6287112f2897b1546
Leslie Shea <leslieyshea@gmail.com>
2019-04-19 11:10:16 -0700

    Film model and test

---
+++ test/models/Film.test.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Film = require('../../lib/models/Film');
+const chance = require('chance').Chance();
+
+describe('film model', () => {
+  it('has a title, studio, released, cast', () => {
+    const film = new Film({
+      title: 'Dog Movie',
+      studio: new mongoose.Types.ObjectId(),
+      released: 1999,
+      cast: [{
+        role: 'Stunt Double',
+        actor: new mongoose.Types.ObjectId()
+      }]
+    });
+    expect(film.toJSON()).toEqual({
+      title: 'Dog Movie',
+      studio: expect.any(mongoose.Types.ObjectId),
+      released: 1999,
+      cast: [{
+        role: 'Stunt Double',
+        actor: expect.any(mongoose.Types.ObjectId),
+        _id: expect.any(mongoose.Types.ObjectId)
+      }],
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+  });
+
+  it('requires a title', () => {
+    const film = new Film({
+      cast: [{ role: 'something' }]
+    }); 
+    const errors = film.validateSync().errors;
+    expect(errors.title.message).toEqual('Path `title` is required.');
+    expect(errors.released.message).toEqual('Path `released` is required.');
+    expect(errors.studio.message).toEqual('Path `studio` is required.');
+    expect(errors['cast.0.actor'].message).toEqual('Path `actor` is required.');
+  });
+});
