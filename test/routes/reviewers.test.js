70d991d08b68658bdfb5bcedfd062e8a035238c8
Parker Helland <hellandp@gmail.com>
2019-04-19 09:37:08 -0700

    reviewer routes written/tests pass

--- test/routes/reviewers.test.js
+++ test/routes/reviewers.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../../lib/app');
-// const Reviewer = require('../../lib/Models/Reviewer');
+const Reviewer = require('../../lib/Models/Reviewer');
 require('../data-helpers');
 
 
@@ -18,5 +18,58 @@ describe('revievver routes', () => {
         expect(res.body).toEqual({ ...jerkface, _id: expect.any(String), __v: 0 });
       });
   });
-  
+
+  it('can get all reviewers', () => {
+    return Reviewer.create(jerkface)
+      .then(() => {
+        return request(app)
+          .get('/api/v1/reviewers')
+          .then(res => {
+            expect(res.body).toHaveLength(1);
+          });
+      });
+  });
+
+  it('find reviewers by id', () => {
+    return Reviewer.create(jerkface)
+      .then(created=>{
+        const id = created._id;
+        return request(app)
+          .get(`/api/v1/reviewers/${id}`)
+          .then(received=>{
+            expect(received.body).toEqual({ ...jerkface, _id:expect.any(String) });
+          });
+      });
+  });
+  it('can update reviewer by id', () => {
+    return Reviewer.create(jerkface)
+      .then(created => {
+        return request(app)
+          .patch(`/api/v1/reviewers/${created._id}`)
+          .send({
+            name: 'better name',
+            company: 'new company'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'better name',
+          company: 'new company',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('deletes a reviewr by id', () => {
+    return Reviewer.create(jerkface)
+      .then(created=>{
+        const id = created._id;
+        return request(app)
+          .delete(`/api/v1/reviewers/${id}`)
+          .then(received=>{
+            expect(received.body).toEqual({ ...jerkface, _id:expect.any(String) });
+          });
+      });
+  });
+
 });
