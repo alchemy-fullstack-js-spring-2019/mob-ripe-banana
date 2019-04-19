0ce10068140ac23b47ec1e86e5000d4121c73996
Parker Helland <hellandp@gmail.com>
2019-04-19 15:03:21 -0700

    review scaffolding made

---
+++ test/models/Review.test.js
@@ -0,0 +1,8 @@
+const Review = require('../../lib/Models/Review');
+const mongoose = require('mongoose');
+
+describe('Review Model Tests', () => {
+  it('has rating, reviewer, review, film, createdAt, updatedAt', () => {
+    
+  });
+});
