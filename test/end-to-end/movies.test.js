464a47dc8797148d48f23b4b2f70d0ce6e092f25
Laura Badgley <l.badgleyy@gmail.com>
2019-04-19 11:36:31 -0700

    first movie route passing

--- test/end-to-end/movies.test.js
+++ test/end-to-end/movies.test.js
@@ -1,21 +1,49 @@
-require('dotenv').config();
-const connect = require('../../lib/utils/connect');
-const mongoose = require('mongoose');
+const { getActor, getMovie, getStudio } = require('../dataHelpers');
 const request = require('supertest');
 const app = require('../../lib/app');
-const Studio = require('../../lib/models/Studio');
-const Actor = require('../../lib/models/Actor');
 
 describe('movie route tests', () => {
-  //create studio and send to mongodb?
-  //create actor and send to mongodb?
-  
+
+
   it('creates a movie', () => {
-    return request(app)
-      .post('/movies')
-      .send({
-        title: 'An Affair To Forget',
-        studio: 
-      })
-  })
-})
+    return Promise.all([
+      getStudio(),
+      getActor()
+    ])
+      .then(([studio, actor]) => {
+        return request(app)
+          .post('/movies')
+          .send({
+            title: 'An Affair To Forget',
+            studio: studio._id,
+            released: 1999,
+            cast: [
+              {
+                role: 'Shirley Temple',
+                actor: actor._id
+              }
+            ]
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.any(String),
+              title: 'An Affair To Forget',
+              studio: expect.any(String),
+              released: 1999,
+              cast: [
+                {
+                  _id: expect.any(String),
+                  role: 'Shirley Temple',
+                  actor: expect.any(String)
+                }
+              ],
+              __v: 0
+            });
+          });
+          
+
+      });
+
+
+  });
+});
