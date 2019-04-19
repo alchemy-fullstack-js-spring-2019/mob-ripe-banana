433e45ee995593844bfe117f9a2c35ba07def8fb
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-19 10:36:22 -0700

    update reviewer

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 require('../../lib/utils/connect')();
 const app = require('../../lib/app');
 
-describe('reviewer route tests', () => {
+describe.only('reviewer route tests', () => {
 
   it('creates a reviewer', () => {
     return request(app)
@@ -23,4 +23,72 @@ describe('reviewer route tests', () => {
       });
   });
 
-})
+  it('gets a list of reviewers', () => {
+    return request(app)
+      .get('/api/v1/reviewers')
+      .then(res => {
+        expect(res.body).toHaveLength(6);
+      });
+  });
+
+  it('gets a reviewer by id', () => {
+    return getReviewer()
+      .then(reviewer => {
+        return Promise.all([
+          Promise.resolve(reviewer),
+          request(app)
+            .get(`/api/v1/reviewers/${reviewer._id}`)
+        ]);
+      })
+      .then(([reviewer, res]) => {
+        expect(res.body).toEqual({
+          name: reviewer.name,
+          company: reviewer.company,
+          _id: reviewer._id
+        });
+      });
+  });
+
+  it('updates reviewer name', () => {
+    return getReviewer()
+      .then(reviewer => {
+        return Promise.all([
+          Promise.resolve(reviewer),
+          request(app)
+            .patch(`/api/v1/reviewers/${reviewer._id}`)
+            .send({
+              name: 'Whatever'
+            })
+        ]);
+      })
+      .then(([reviewer, res]) => {
+        expect(res.body).toEqual({
+          _id: reviewer._id,
+          name: 'Whatever',
+          company: reviewer.company
+        });
+      });
+  });
+
+  it('updates reviewer company', () => {
+    return getReviewer()
+      .then(reviewer => {
+        return Promise.all([
+          Promise.resolve(reviewer),
+          request(app)
+            .patch(`/api/v1/reviewers/${reviewer._id}`)
+            .send({
+              company: 'Whatever'
+            })
+        ]);
+      })
+      .then(([reviewer, res]) => {
+        expect(res.body).toEqual({
+          _id: reviewer._id,
+          name: reviewer.name,
+          company: 'Whatever'
+        });
+      });
+  });
+
+});
