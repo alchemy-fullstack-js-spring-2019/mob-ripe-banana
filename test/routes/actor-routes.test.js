d1245f7e2d332a6b0b55bdde00e1ddc5756384bf
Tommy Tran <tranttommy95@gmail.com>
2019-04-22 11:53:44 -0700

    will not delete actor if in film, cool

--- test/routes/actor-routes.test.js
+++ test/routes/actor-routes.test.js
@@ -1,22 +1,8 @@
-require('dotenv').config();
 const request = require('supertest');
-const mongoose = require('mongoose');
 const app = require('../../lib/app');
-const connect = require('../../lib/utils/connect');
+const { getActor } = require('../data-helper');
 
 describe('Actor routes tests', () => {
-  beforeAll(() => {
-    return connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
   it('creates an Actor', () => {
     return request(app)
       .post('/api/v1/actors')
@@ -46,9 +32,10 @@ describe('Actor routes tests', () => {
       })
       .then(() => request(app).get('/api/v1/actors'))
       .then(res => {
-        expect(res.body).toHaveLength(1);
+        expect(res.body).toHaveLength(101);
       });
   });
+
   it('gets an actor by id', () => {
     return request(app)
       .post('/api/v1/actors')
@@ -109,6 +96,19 @@ describe('Actor routes tests', () => {
           dob: new Date('9/3/1965').toISOString(),
           pob: 'New York City',
           _id: expect.any(String)
+        });
+      });
+  });
+  
+  it('does not delete an actor if they are in films', ()=> {
+    return getActor()
+      .then(actor => {
+        return request(app)
+          .delete(`/api/v1/actors/${actor._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          error:'Cannot delete actor, while films still exist'
         });
       });
   });
